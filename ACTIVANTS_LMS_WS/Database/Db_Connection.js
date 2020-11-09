c35f758d863a25e0f17c5b6d673d137611fe9594
tharine ramachandran <54879683+tharineramachandran@users.noreply.github.com>
2020-11-09 12:41:49 +0800

    Update Db_Connection.js

--- ACTIVANTS_LMS_WS/Database/Db_Connection.js
+++ ACTIVANTS_LMS_WS/Database/Db_Connection.js
@@ -1,9 +1,9 @@
 const Pool = require('pg').Pool;
 
 const pool  = new Pool({
-    user: "postgres",
-    password: "1234",
-    host: "localhost",
+    user: "LMScounsellor",
+    password: "lmscounsellor",
+    host: "lmscounsellor.cgepka50vsag.us-east-2.rds.amazonaws.com",
     port:5432,
     database:"ACTIVANTS_LMS_COUNSELLOR",
 });
