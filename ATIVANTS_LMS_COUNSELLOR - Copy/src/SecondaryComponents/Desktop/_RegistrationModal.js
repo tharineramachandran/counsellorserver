e288bb58933e81235e83a245f3911362fec707ad
tharine ramachandran <54879683+tharineramachandran@users.noreply.github.com>
2020-11-23 00:37:38 +0800

    upfate
    upfate

--- ATIVANTS_LMS_COUNSELLOR - Copy/src/SecondaryComponents/Desktop/_RegistrationModal.js
+++ ATIVANTS_LMS_COUNSELLOR - Copy/src/SecondaryComponents/Desktop/_RegistrationModal.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect, useContext } from 'react';
 import {
-    Header, Icon, Form, Image, Message,
+    Header, Icon, Form, Image, Message,Container,
     Segment, Grid, Modal, Search, Button, Dimmer, Loader, Checkbox, Divider, List, Label
 } from 'semantic-ui-react';
 import Counsellor_Registration from './_Counsellor_Registration';
 
 
+import ReCAPTCHA from "react-google-recaptcha";
+import keys from "../../env";
 
 import ChangePassword from "./UserDashboard/layout/ChangePasswordModel"
 import { Authorize } from "../../MainComponents/DesktopComponent";
@@ -19,6 +21,8 @@ import { baseURLAPI, baseURL } from "../../Global";
 
 
 const _RegistrationModal = props => {
+     
+    const [ TermsOpen, setTermsOpen] = useState(false);
     const [open, setOpen] = useState(true);
     const [defHeight, setHeight] = useState(window.innerWidth);
     const [ModalLogin, setModalLogin] = useState(true);
@@ -58,7 +62,7 @@ const _RegistrationModal = props => {
 
     const onSubmitStudentSignUpForm = async (e) => {
         e.preventDefault();
-
+        if (  recapcha.value) {
         try {
             const body = { TX_USER_NAME, TX_USER_EMAIL, TX_USER_PASSWORD };
             console.log(body);
@@ -101,6 +105,13 @@ const _RegistrationModal = props => {
         } catch (error) {
             console.log(error.message);
         }
+    }
+        else {
+            setErrorSignUpMessage("Check Recapcha before submit");
+            setAuth(false);
+        }
+
+
     }
 
 
@@ -171,7 +182,22 @@ const _RegistrationModal = props => {
             window.removeEventListener('resize', setHeightofScreen)
         }
     })
+    const [recapcha, setrecapcha] = useState([]);
 
+    
+
+  const  handleChange = async (value) =>    {
+        console.log("Captcha value:", value);
+        setrecapcha({ value });
+        // if value is null recaptcha expired
+        if (value === null) this.setState({ expired: "true" });
+      };
+    
+    const  asyncScriptOnLoad = async () => {
+        setrecapcha({ callback: "called!" });
+        console.log("scriptLoad - reCaptcha Ref-" );
+      };
+     
     const StudentSignUp = () => {
         setModalLogin(false)
         setModalStudentSignUp(true)
@@ -450,7 +476,66 @@ const _RegistrationModal = props => {
                                                         onChange={e => onChangeOfForm(e)}
                                                         value={TX_USER_PASSWORD}
                                                     />
-                                                </Form.Field>
+
+                                                    
+                                                </Form.Field></Form.Group>
+                                                <Form.Group widths='equal'> 
+                                                <Form.Field>
+                                                <ReCAPTCHA
+                            sitekey={keys.google.googleRecapcha }
+
+                            style={{ display: "inline-block" }}
+                            theme="light"
+                          
+                            onChange={handleChange}
+                            asyncScriptOnLoad={asyncScriptOnLoad}
+                        /></Form.Field></Form.Group>  <Form.Group widths='equal'> 
+                         <Form.Field> 
+
+                         
+                                              
+                         <Modal
+                                                    open={TermsOpen}
+                                                    onClose= {() => setTermsOpen(false)}
+                                                    onOpen={() => setTermsOpen(true)}
+                                                    trigger={
+
+
+                                                        <p>
+                                                          By clicking Sign-up, you agree to the counselling gorilla's terms of service  and privacy policy 
+    </p>}
+                                                >
+                                                    <Modal.Header>Terms and Conditions</Modal.Header>
+                                                    <Modal.Content image scrolling>
+
+                                                        <Modal.Description>
+                                                            <Container width='20%' text>
+
+                                                                <p>
+
+                                                                    Welcome to Counselling LMS!
+ <br /> <br />
+These terms and conditions outline the rules and regulations for the use of Counselling LMS's Website, located at CounsellingLMS.com.
+
+By accessing this website we assume you accept these terms and conditions. Do not continue to use Counselling LMS if you do not agree to take all of the terms and conditions stated on this page.
+
+The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.
+<br />
+Cookies <br />  </p>
+
+                                                            </Container>
+
+                                                        </Modal.Description>
+                                                    </Modal.Content>
+                                                    <Modal.Actions>
+                                                        <Button onClick={() => setTermsOpen(false)}  >
+                                                            Close
+        </Button>
+                                                    </Modal.Actions>
+                                                </Modal>
+
+                         </Form.Field>
+
                                             </Form.Group>
                                             {errorSignUpMessage && (
                                                 <Form.Group widths='equal'>
